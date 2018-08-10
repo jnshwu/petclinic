0d301658c7d36daf972580c147c206e1f6165716
jinshiwu <jnshwu@gmail.com>
2018-08-10 12:49:15 -0500

    Init project commit

---
+++ src/main/java/com/jinshi/petclinic/JinshiPetclinicApplication.java
@@ -0,0 +1,12 @@
+package com.jinshi.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinshiPetclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinshiPetclinicApplication.class, args);
+	}
+}
