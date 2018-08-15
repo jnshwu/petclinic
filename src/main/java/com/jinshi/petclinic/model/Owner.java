570f228bf806409d794e4e50a8cb035af1291e1a
jinshiwu <jnshwu@gmail.com>
2018-08-15 12:36:01 -0500

    adding POJOs

---
+++ src/main/java/com/jinshi/petclinic/model/Owner.java
@@ -0,0 +1,7 @@
+package com.jinshi.petclinic.model;
+
+/**
+ * Created by jinshiwu on 8/15/18.
+ */
+public class Owner extends Person {
+}
---
+++ src/main/java/com/jinshi/petclinic/model/Vet.java
@@ -0,0 +1,7 @@
+package com.jinshi.petclinic.model;
+
+/**
+ * Created by jinshiwu on 8/15/18.
+ */
+public class Vet extends Person {
+}
